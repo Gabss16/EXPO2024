01228830c64c9c3afbb0143b333b4547adfb78c1
gabriela <20200543@ricaldone.edu.sv>
2024-06-03 15:26:49 -0600

    SplahScreen

--- app/src/main/java/com/example/expogbss/MainActivity.kt
+++ app/src/main/java/com/example/expogbss/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.expogbss
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch (Dispatchers.Main){
+            delay(3000)
+            val pantallaLogin = Intent(this@MainActivity, login::class.java)
+            startActivity(pantallaLogin)
+        }
+
     }
+
+
 }
