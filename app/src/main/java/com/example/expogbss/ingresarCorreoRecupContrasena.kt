9114e799296545ca7467cf0c9e6b404455386f12
GabContreras <contrerasgab01@gmail.com>
2024-07-02 14:01:14 -0600

    Intento de corrección activity registro empresa

--- app/src/main/java/com/example/expogbss/ingresarCorreoRecupContrasena.kt
+++ app/src/main/java/com/example/expogbss/ingresarCorreoRecupContrasena.kt
@@ -33,63 +33,74 @@ class ingresarCorreoRecupContrasena : AppCompatActivity() {
         val correoRecuperacion = findViewById<EditText>(R.id.txtIngresarCorreoRecuperacion)
         var codigoRecuperacion = (1000..9999).random()
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val objConexion = ClaseConexion().cadenaConexion()
 
+            btnEnviar.setOnClickListener {
+                GlobalScope.launch(Dispatchers.IO) {
+                val objConexion = ClaseConexion().cadenaConexion()
 
-            val comprobarCredencialesSiEsEmpresa =
-                objConexion?.prepareStatement("SELECT * FROM EMPLEADOR WHERE CorreoElectronico = ?")!!
-            comprobarCredencialesSiEsEmpresa.setString(1, correoRecuperacion.text.toString())
+                val comprobarCredencialesSiEsEmpresa =
+                    objConexion?.prepareStatement("SELECT * FROM EMPLEADOR WHERE CorreoElectronico = ?")!!
+                comprobarCredencialesSiEsEmpresa.setString(1, correoRecuperacion.text.toString())
 
-            val comprobarCredencialesSiEsSolicitante =
-                objConexion?.prepareStatement("SELECT * FROM SOLICITANTE WHERE CorreoElectronico = ?")!!
-            comprobarCredencialesSiEsSolicitante.setString(1, correoRecuperacion.text.toString())
+                val comprobarCredencialesSiEsSolicitante =
+                    objConexion?.prepareStatement("SELECT * FROM SOLICITANTE WHERE CorreoElectronico = ?")!!
+                comprobarCredencialesSiEsSolicitante.setString(
+                    1,
+                    correoRecuperacion.text.toString()
+                )
 
-            val esEmpresa = comprobarCredencialesSiEsEmpresa.executeQuery()
-            val esSolicitante = comprobarCredencialesSiEsSolicitante.executeQuery()
+                val esEmpresa = comprobarCredencialesSiEsEmpresa.executeQuery()
+                val esSolicitante = comprobarCredencialesSiEsSolicitante.executeQuery()
 
-            if (esEmpresa.next()) {
-                btnEnviar.setOnClickListener {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        val correoEnviado = recuperarContrasena(correoRecuperacion.text.toString(), "Recuperación de contraseña", "Hola este es su codigo de recuperacion: $codigoRecuperacion")
-
-                        if (correoEnviado) {
-                            val pantallaIngresoCodigo = Intent(this@ingresarCorreoRecupContrasena, recoveryCode::class.java).apply {
-                                putExtra("codigoRecuperacion", codigoRecuperacion)
-                                putExtra("correo", correoRecuperacion.text.toString())
+                if (esEmpresa.next()) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val correoEnviado = recuperarContrasena(
+                                correoRecuperacion.text.toString(),
+                                "Recuperación de contraseña",
+                                "Hola este es su codigo de recuperacion: $codigoRecuperacion"
+                            )
+                            if (correoEnviado) {
+                                val pantallaIngresoCodigo = Intent(
+                                    this@ingresarCorreoRecupContrasena,
+                                    recoveryCode::class.java
+                                ).apply {
+                                    putExtra("codigoRecuperacion", codigoRecuperacion)
+                                    putExtra("correo", correoRecuperacion.text.toString())
+                                }
+                                startActivity(pantallaIngresoCodigo)
+                                finish()
+                            } else {
+                                println("PapuError")
                             }
-                            startActivity(pantallaIngresoCodigo)
-                            finish()
-                        } else {
-                            println("PapuError")
                         }
+                }
+                if (esSolicitante.next()) {
+                        CoroutineScope(Dispatchers.Main).launch {
+                            println("entra a la corrutina")
+                            val correoEnviado = recuperarContrasena(
+                                correoRecuperacion.text.toString(),
+                                "Recuperación de contraseña",
+                                "Hola este es su codigo de recuperacion: $codigoRecuperacion"
+                            )
+
+                            if (correoEnviado) {
+                                val pantallaIngresoCodigo = Intent(
+                                    this@ingresarCorreoRecupContrasena,
+                                    recoveryCode::class.java
+                                ).apply {
+                                    putExtra("codigoRecuperacion", codigoRecuperacion)
+                                    putExtra("correo", correoRecuperacion.text.toString())
+                                }
+                                startActivity(pantallaIngresoCodigo)
+                                finish()
+                            } else {
+                                println("PapuError")
+                            }
                     }
+                } else {
+                    println("Correo no encontrado, ingrese uno válido")
                 }
             }
-            if(esSolicitante.next()){
-                btnEnviar.setOnClickListener {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        val correoEnviado = recuperarContrasena(correoRecuperacion.text.toString(), "Recuperación de contraseña", "Hola este es su codigo de recuperacion: $codigoRecuperacion")
-
-                        if (correoEnviado) {
-                            val pantallaIngresoCodigo = Intent(this@ingresarCorreoRecupContrasena, recoveryCode::class.java).apply {
-                                putExtra("codigoRecuperacion", codigoRecuperacion)
-                                putExtra("correo", correoRecuperacion.text.toString())
-                            }
-                            startActivity(pantallaIngresoCodigo)
-                            finish()
-                        } else {
-                            println("PapuError")
-                        }
-                    }
-
-                }            }
-            else
-            {
-                println("Correo no encontrado, ingrese uno válido")
-            }
         }
-    }
-
     }
     }
