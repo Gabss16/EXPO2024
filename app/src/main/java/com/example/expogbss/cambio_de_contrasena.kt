a8518b186593bcc836a06c97b0e6087a1a70fccf
GabContreras <contrerasgab01@gmail.com>
2024-07-09 15:40:43 -0600

    Ya sirve el login, ver como quitar viñeta arriba de cada fragment

--- app/src/main/java/com/example/expogbss/cambio_de_contrasena.kt
+++ app/src/main/java/com/example/expogbss/cambio_de_contrasena.kt
@@ -1,12 +1,15 @@
 package com.example.expogbss
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import modelo.ClaseConexion
+import java.security.MessageDigest
 
 class cambio_de_contrasena : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,22 +21,33 @@ class cambio_de_contrasena : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //Códigos que se traen de la pantalla anterior
+        val correo = ingresarCorreoRecupContrasena.correoIngresado
+        val codigoRecuperacion = ingresarCorreoRecupContrasena.codigo
 
-        
-        val verificarContraseña =
-            Regex("^(?=.*[0-9!@#\$%^&*()-_=+\\|\\[{\\]};:'\",<.>/?]).{6,}\$")
+        val btnCambiarContrasena = findViewById<Button>(R.id.btnCambiarContrasena)
+        val txtContrasenaNueva = findViewById<EditText>(R.id.txtnuevacontrasena)
+
+        val verificarContraseña = Regex("^(?=.*[0-9!@#\$%^&*()-_=+\\|\\[{\\]};:'\",<.>/?]).{6,}\$")
+
+        //Creo la función para encriptar la contraseña
+        fun hashSHA256(contraseniaEscrita: String): String {
+            val bytes =
+                MessageDigest.getInstance("SHA-256").digest(contraseniaEscrita.toByteArray())
+            return bytes.joinToString("") { "%02x".format(it) }
+        }
 
 
+        btnCambiarContrasena.setOnClickListener {
+            val contrasenaNueva = txtContrasenaNueva.text.toString()
 
-        if (newPassword.isEmpty()) {
+        if (contrasenaNueva.isEmpty()) {
             Toast.makeText(
-                this@recoveryCode,
-                "La contraseña no puede estar vacía",
-                Toast.LENGTH_SHORT
+                this@cambio_de_contrasena, "La contraseña no puede estar vacía", Toast.LENGTH_SHORT
             ).show()
-        } else if (!verificarContraseña.matches(newPassword)) {
+        } else if (!verificarContraseña.matches(contrasenaNueva)) {
             Toast.makeText(
-                this@recoveryCode,
+                this@cambio_de_contrasena,
                 "La contraseña debe tener al menos 6 caracteres y un carácter especial",
                 Toast.LENGTH_SHORT
             ).show()
@@ -41,7 +55,7 @@ class cambio_de_contrasena : AppCompatActivity() {
             val objConexion = ClaseConexion().cadenaConexion()
 
             // Encripto la contraseña usando la función de encriptación
-            val contrasenaEncriptada = hashSHA256(newPassword)
+            val contrasenaEncriptada = hashSHA256(contrasenaNueva)
 
             val actualizarcontraseña =
                 objConexion?.prepareStatement("UPDATE EMPLEADOR SET contrasena = ? WHERE CorreoElectronico = ?")!!
@@ -49,8 +63,7 @@ class cambio_de_contrasena : AppCompatActivity() {
             actualizarcontraseña.setString(2, correo)
             actualizarcontraseña.executeQuery()
 
-            // Si todo está bien, cerrar el AlertDialog
-            alertDialog.dismiss()
         }
+    }
     }
 }
