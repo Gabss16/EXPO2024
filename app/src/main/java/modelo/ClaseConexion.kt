5fb538068f540c7a629e46df811e5fac2e57338e
Marcos López <la.galletapez@gmail.com>
2024-07-10 18:12:08 -0600

    Actualizacion perfil

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -6,16 +6,18 @@ import java.sql.DriverManager
 class ClaseConexion {
 
     fun cadenaConexion(): Connection? {
-        try {
-            val ip = "jdbc:oracle:thin:@192.168.0.17:1521:xe"
-            val usuario = "GABRIEL"
-            val contrasena = "hKAKI88v"
+        return try {
+            val ip = "jdbc:oracle:thin:@112.13.14.27:1521:xe"
+            val usuario = "SYSTEM"
+            val contrasena = "ITR2024"
 
+            // Attempt to establish a connection
             val conexion = DriverManager.getConnection(ip, usuario, contrasena)
-            return conexion
-        }catch (e: Exception){
+            conexion
+        } catch (e: Exception) {
+            // Print the error message if an exception occurs
             println("El error es este: $e")
-            return null
+            null
         }
     }
 }
