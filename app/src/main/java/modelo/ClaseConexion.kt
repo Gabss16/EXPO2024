f49c0d93a9eeb23b92cf7419c16f3ba69de70da5
Max <20200176@ricaldone.edu.sv>
2024-07-14 16:15:54 -0600

    pruebas

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -7,16 +7,17 @@ class ClaseConexion {
 
     fun cadenaConexion(): Connection? {
         return try {
-            val ip = "jdbc:oracle:thin:@192.168.1.15:orcl"
+            val ip = "jdbc:oracle:thin:@192.168.1.12:1521:xe"
             val usuario = "SYSTEM"
-            val contrasena = "ITR2024"
+            val contrasena = "Halcon$200"
 
-            // Intentar establecer una conexión
-            DriverManager.getConnection(ip, usuario, contrasena)
+            // Attempt to establish a connection
+            val conexion = DriverManager.getConnection(ip, usuario, contrasena)
+            conexion
         } catch (e: Exception) {
-            // Imprimir el mensaje de error si ocurre una excepción
+            // Print the error message if an exception occurs
             println("El error es este: $e")
             null
-            }
+        }
     }
 }
