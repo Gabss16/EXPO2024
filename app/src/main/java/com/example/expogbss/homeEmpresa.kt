3178265198b4590e4d1d3b5b679ec41c0463ba96
Gabriela <20200543@ricaldone.edu.sv>
2024-08-31 10:27:53 -0600

    botones eliminar y actualizar

--- app/src/main/java/com/example/expogbss/homeEmpresa.kt
+++ app/src/main/java/com/example/expogbss/homeEmpresa.kt
@@ -300,7 +300,7 @@ class homeEmpresa : Fragment() {
 
                         //2-creo una variable que contenga un PrepareStatement
                         val addTrabajo =
-                            objConexion?.prepareStatement("INSERT INTO TRABAJO ( Titulo , IdEmpleador , AreaDeTrabajo,Descripcion ,Ubicacion , Experiencia , Requerimientos , Estado ,Salario , Beneficios, FechaDePublicacion ) VALUES (  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,? )")!!
+                            objConexion?.prepareStatement("INSERT INTO TRABAJO ( Titulo , IdEmpleador , AreaDeTrabajo,Descripcion ,Ubicacion ,idDepartamento, Experiencia , Requerimientos , Estado ,Salario , Beneficios, FechaDePublicacion ) VALUES (  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,? )")!!
                         addTrabajo.setString(1, txtTituloJob.text.toString())
                         addTrabajo.setString(2, idEmpleador)
                         addTrabajo.setInt(3, idAreaDeTrabajo)
