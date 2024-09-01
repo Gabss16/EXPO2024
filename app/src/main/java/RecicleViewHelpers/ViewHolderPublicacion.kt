436cc46cc4178b4863e42d2623d4257224f2ae13
Franklin <falemelgar@gmail.com>
2024-08-31 22:48:26 -0600

    Medio arregle la ñparte de editar

--- app/src/main/java/RecicleViewHelpers/ViewHolderPublicacion.kt
+++ app/src/main/java/RecicleViewHelpers/ViewHolderPublicacion.kt
@@ -1,6 +1,7 @@
 package RecicleViewHelpers
 
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.expogbss.R
@@ -8,5 +9,5 @@ import com.example.expogbss.R
 class ViewHolderPublicacion (view: View): RecyclerView.ViewHolder(view) {
     var txtTIituloTrabajo= view.findViewById<TextView>(R.id.txtTituloTrabajo)
     var txtTipoTrabajo =  view.findViewById<TextView>(R.id.txtTipoTrabajo)
-    var imgBorrar =  view.findViewById<TextView>(R.id.txtTipoTrabajo)
-    var imgEditar =  view.findViewById<TextView>(R.id.txtTipoTrabajo)}
+    var imgBorrar =  view.findViewById<ImageButton>(R.id.imgBorrarPublicacion)
+    var imgEditar =  view.findViewById<ImageButton>(R.id.imgEditarPublicacion)}
